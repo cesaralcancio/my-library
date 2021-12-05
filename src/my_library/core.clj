26b9fab10fb3e633ff813899e55935c296eabaf0
Cesar Alcancio <cesar.alcancio@gmail.com>
2021-12-05 12:27:44 -0300

    second commit

---
+++ src/my_library/core.clj
@@ -0,0 +1,12 @@
+(ns my-library.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(defn hello-world
+  "This is my first function in the library"
+  []
+  (println "Hello, World!"))
